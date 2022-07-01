83dc10d92328de464711b51897ae6ee1300b3a53
Андрей Жиянов <aazhiyanov@gmail.com>
2022-07-01 16:19:39 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TransformerLoadLosses;
+
+namespace LoadLossChart
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WPFDiagram.Plot.Title("График нагрузочных потерь трансформатора");
+
+            Transformator PRIME = new Transformator(55, 16000, 14805, 18175);
+            Transformator SECOND = new Transformator(55, 16000, 9750, 32390);
+            int step = 800;
+            int lengh = PRIME.Snom;
+
+
+            double[] dataX = Transformator.LoadLossesTrans(PRIME);
+            double[] dataY = Transformator.LoadLossesTrans(PRIME,SECOND);
+            double[] sine = DataGen.Sin(600, 3);
+            WPFDiagram.Plot.AddScatter(dataX, dataY);
+            WPFDiagram.Refresh();
+        }
+    }
+}
---
+++ Transformator.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransformerLoadLosses
+{
+    class Transformator
+    {
+
+        private double U;
+
+        public double Unom
+        {
+            get { return U; }
+            set
+            {
+                if (value < 0) U = 0;
+                else U = value;
+            }
+        }
+        private double S;
+
+        public double Snom
+        {
+            get { return S; }
+            set
+            {
+                if (value < 0) S = 0;
+                else S = value;
+            }
+        }
+
+        public double Pxx { get; set; }
+        public double Pкз { get; set; }
+
+
+        public Transformator(int U, int S, int Pxx, int Pкз)
+        {
+            Unom = U;
+            Snom = S;
+            this.Pxx = Pxx;
+            this.Pкз = Pкз;
+
+        }
+
+        //перегруженный метод создает массив точек для построения графика нагрузочных потерь одного трансформатора,
+
+        public static double[] LoadLossesTrans(Transformator PRIME)
+        {
+            int step = 800;
+           
+            double[] VariablePower = new double[(int)PRIME.Snom * 2 + 1];
+
+            for (int i = 0; i <= PRIME.Snom * 2; i += step)
+            {
+                VariablePower[i] = i;
+            }
+
+            return VariablePower;
+        }
+
+        public static double[] LoadLossesOneTrans(Transformator PRIME)
+        {
+            int step = 800;
+            double[] ArrOneTrans = new double[(int)PRIME.Snom * 2 + 1];
+
+
+            for (int i = 0; i <= PRIME.Snom * 2; i += step)
+            {
+                //формула для графика нагрузочных потерь одного трансформатора
+                ArrOneTrans[i] = Math.Pow(i / PRIME.Snom, 2) * PRIME.Pкз + PRIME.Pxx;
+
+            }
+
+            return ArrOneTrans;
+        }
+
+        //метод создает массив точек для построения графика нагрузочных потерь двух параллельных трансформатора
+        public static double[] LoadLossesTrans(Transformator PRIME, Transformator SECOND)
+        {
+            int step = 800;
+            double[] ArrTwoTrans = new double[(int)PRIME.Snom * 2 + 1];
+            int[] VariablePower = new int[(int)PRIME.Snom * 2 + 1];
+
+            for (int i = 0; i <= PRIME.Snom * 2; i += step)
+            {
+                ArrTwoTrans[i] = Math.Pow(i / PRIME.Snom, 2) * (PRIME.Pкз * SECOND.Pкз / (PRIME.Pкз + SECOND.Pкз)) + (PRIME.Pxx + SECOND.Pxx);
+                VariablePower[i] = i;
+            }
+
+            return ArrTwoTrans;
+        }
+
+
+    }
+}
